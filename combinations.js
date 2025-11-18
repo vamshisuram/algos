70b65febcb75bb746e51721fe1b44a26a727aeda
Vamshi Suram <vamshi.suram25@gmail.com>
2025-11-18 10:02:19 +0530

    combinations

---
+++ combinations.js
@@ -0,0 +1,27 @@
+// n = 4, k = 2
+
+function combinations(n, k) {
+
+    const result = []
+    const curr = []
+
+    function backtrack(start) {
+        // base
+        if (curr.length === k) {
+            result.push([...curr])
+            return;
+        }
+
+        for (let i = start; i <= 4; i++) {
+            curr.push(i);
+            backtrack(i + 1);
+            curr.pop();
+        }
+
+    }
+
+    backtrack(1);
+    return result;
+}
+
+console.log(JSON.stringify(combinations(4, 2)))
